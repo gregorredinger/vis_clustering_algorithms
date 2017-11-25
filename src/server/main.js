68e8f056a065974ae854d96aa3f5aafa654f454e
gregor <gregorredinger@gmail.com>
2017-11-25 22:43:12 +0100

    try to fix broken github-pages website

--- src/server/main.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-// start server
-app.listen(8080, () => { console.log("server started..."); });
-
-// serve static files with express, access via http://domain:port/filename
-// @see http://expressjs.com/de/starter/static-files.html
-app.use(express.static("dist"));
