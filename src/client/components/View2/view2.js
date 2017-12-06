13e88298fafde3c56e1adba7431d8449c9727124
gregor <gregorredinger@gmail.com>
2017-12-06 21:08:23 +0100

    !!! PLEASE UPDATE DEPENDENCIES WITH 'npm update' - because new dependency 'handsontable' was installed - handsontable spreadsheet works so far, but there is a issue with the CSS Grid - The Spreadsheet gets inserted with a fixed width and height (e.g. 500px), but because css grid seems to resizes according to its content, the containing grid gets larger.

--- src/client/components/View2/view2.js
+++ src/client/components/View2/view2.js
@@ -1,27 +1,75 @@
 import "./view2.scss";
+import "./hot.scss"; // css for handsontable module
 import Handsontable from "handsontable";
+import Store from "../../store";
 
-
+    /**
+ * @module View2
+ * This module contains the diagrams for View 2
+ * */
 export default class {
 
     constructor() {
+        // calculate the size of the parent node where the Spreadsheet gets inserted
+        this.widthOfSpreadsheetContainer = document.getElementById('view2_spreadsheet').parentNode.getBoundingClientRect().width;
+        this.heightOfSpreadsheetContainer = document.getElementById('view2_spreadsheet').parentNode.getBoundingClientRect().height;
+        this.store = new Store();
 
     }
 
+    /**
+     * draws the Spreadsheet
+     *
+     * @requires Handsontable - {@link https://handsontable.com/examples?headers#|Handsontable Spreadsheet Example}
+     *
+     * @see {@link https://handsontable.com/blog/articles/a-complete-guide-to-changing-size-of-handsontable|Change Size of Handsontable}
+     * */
     drawSpreadsheet() {
-        var data = [
-            ["", "Tesla", "Volvo", "Toyota", "Honda"],
-            ["2017", 10, 11, 12, 13],
-            ["2018", 20, 11, 14, 13],
-            ["2019", 30, 15, 12, 13]
-        ];
 
-        var container = document.getElementById('view2_spreadsheet');
-        var hot = new Handsontable(container, {
-            data: data,
+        let hotElement = document.querySelector('#view2_spreadsheet'); // node where the spreadsheet gets inserted
+        let hotSettings = {
+            data: this.store.data,
+            // define which properties of the passed data object should be inserted
+            // 'data' means the key of the propertie in the obj, 'type' means the datatype e.g. numeric, text,...
+            columns: [
+                {
+                    data: 'x',
+                    type: 'numeric',
+                },
+                {
+                    data: 'y',
+                    type: 'numeric',
+                },
+                {
+                    data: 'reachabilityDistance',
+                    type: 'text',
+                },
+                {
+                    data: 'name',
+                    type: 'text',
+                },
+                {
+                    data: 'color',
+                    type: 'text'
+                }
+            ],
+            stretchH: 'all',
+            width: this.widthOfSpreadsheetContainer, // gets pre-calculated in the constructor, takes a integer value
+            autoWrapRow: true,
+            height: this.heightOfSpreadsheetContainer, // gets pre-calculated in the constructor, takes a integer value
+            maxRows: 22,
             rowHeaders: true,
-            colHeaders: true
-        });
+            // define the names of the column headers
+            colHeaders: [
+                'x',
+                'y',
+                'Reachability Distance',
+                'name',
+                'color'
+            ]
+        };
+
+        let hot = new Handsontable(hotElement, hotSettings);
 
     }
 }
